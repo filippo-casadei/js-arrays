580f617b93a42f6b9dfe93141b18a5de9a9a7a62
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-25 16:29:46 +0100

    risolta task 8.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -42,4 +42,5 @@ const lewisIndex = teachers.indexOf("Lewis");
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+//console.log(isTeachersEmpty)
