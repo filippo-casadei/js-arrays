2ced38071db05e7b20ca02051515600b104d1b39
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-25 18:03:57 +0100

    corretta e risolta task bonus 2 e task bonus 3.

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -19,9 +19,16 @@ for (let i = teachers.length -1; i >= 0; i--) {
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = []
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+  longNames.push(teachers[i]);
+  }
+}
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(1,1);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
